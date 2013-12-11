1731f7f982f6dedb818b5ad3f2911b7a828bb239
Roger Enriquez Jr <roger.enriquez@gmail.com>
2013-12-11 09:41:52 -0600

    Initial Commit

---
+++ frontend/app/scripts/app.js
@@ -0,0 +1,8 @@
+angular.module('version_hub'), [])
+    .constant('ROUTE_PATHS', {
+        'homepage': "/",
+        'application'
+    })
+    .config(function($routeProvider, ROUTE_PATHS)) {
+
+    })
